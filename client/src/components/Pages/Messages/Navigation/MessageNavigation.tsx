262460735cc7fb45542f60adcdb2bbeaeb4be544
Jusa Mankki <jusamankki@gmail.com>
2021-06-28 16:10:18 +0300

    mobile scaling for messaging

--- client/src/components/Pages/Messages/Navigation/MessageNavigation.tsx
+++ client/src/components/Pages/Messages/Navigation/MessageNavigation.tsx
@@ -1,11 +1,11 @@
-import { useQuery } from '@apollo/client';
 import React from 'react'
+import { useQuery } from '@apollo/client';
 import { NavLink } from "react-router-dom";
 import { CONVERSATION_INFOS } from '../../../../graphql/queries';
 
 import './MessageNavigation.css'
 
-const MessageNavigation = () => {
+const MessageNavigation = ({setShowContacts}: any) => {
   const result = useQuery(CONVERSATION_INFOS)
 
   if (result.loading) {
@@ -23,7 +23,7 @@ const MessageNavigation = () => {
             const linkTo = `../messages/${conversation.id}`
             return (
               <li className="msg-nav-item" key={conversation.id}>
-                <NavLink exact to={linkTo} activeClassName="msg-active" className="msg-nav-links">
+                <NavLink exact to={linkTo} activeClassName="msg-active" className="msg-nav-links" onClick={() => setShowContacts(false)}>
                   {usernames}
                 </NavLink>
               </li>
