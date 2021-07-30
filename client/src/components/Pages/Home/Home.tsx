a648aac5682eadb4ada35fdaadce33b2a1d72e5c
Jusa Mankki <jusamankki@gmail.com>
2021-07-30 14:59:25 +0300

    home page css improvements

--- client/src/components/Pages/Home/Home.tsx
+++ client/src/components/Pages/Home/Home.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Home.css'
-import Guitarist from '../../../images/beard-musician-guitar-acoustic.jpeg'
 
 const Home = () => {
 
   return (
     <div>
       <nav className="home-page-navigation">
-        <div>
-          <a href="#home" className="home-navigation-link">Top</a>
+        <div className="home-page-navigation-links home-page-scroll-links">
+          <a href="#home" className="home-navigation-link"><i className="fas fa-cookie-bite"></i></a>
           <a href="#about" className="home-navigation-link">About</a>
           <a href="#features" className="home-navigation-link">Features</a>
         </div>
-        <div>
-          <a href="/signin" className="home-navigation-link">Sign In</a>
-          <a href="/signup" className="home-navigation-link">Sign Up</a>
+        <div className="home-page-navigation-links home-page-sign-links">
+          <a href="/signin" className="home-navigation-link sign-link">Sign In</a>
+          <a href="/signup" className="home-navigation-link sign-link">Sign Up</a>
         </div>
       </nav>
       <div className="scroll-container">
@@ -31,7 +30,7 @@ const Home = () => {
           <h1>Features</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ut ipsa nesciunt quod sequi pariatur, cumque, unde esse earum aliquid officiis eligendi? Atque aspernatur sequi tenetur aut qui, incidunt numquam reiciendis neque, laudantium officia deserunt non, eius illum facilis magnam nihil dolor enim. Incidunt repellat ipsum sequi nihil dolor quas obcaecati, necessitatibus iusto eum. Quo voluptatem aspernatur harum totam! Beatae maxime fugit corrupti! Eveniet hic totam doloribus eos recusandae! Quidem culpa tenetur consequatur rem aut voluptatem illo, fugit quaerat quo, quis assumenda tempore asperiores architecto adipisci cum. Repudiandae voluptate exercitationem fugit magnam! Ab ipsa minima eveniet odit amet doloremque soluta!</p>
         </div>
-        <div className="footer-section">
+        <div className="scroll-page footer-section">
           <h1>Footer</h1>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, a assumenda eveniet deleniti itaque amet, vero excepturi alias voluptate cum at obcaecati beatae dolor aperiam tenetur ipsum impedit debitis officia sapiente! Id officiis ex mollitia veritatis. Ullam dignissimos impedit nemo culpa, non sunt perferendis modi officia odit molestiae repellendus? Laboriosam amet quos aspernatur? Rem iste, dolores officiis in, magni neque incidunt minus iusto pariatur eveniet error provident nobis nam animi ullam voluptates velit nihil reiciendis temporibus aut molestias. Quos voluptatibus praesentium, ut nam saepe culpa nisi iusto nostrum earum magni asperiores aspernatur? Voluptas officia fugit in vitae esse sunt placeat.</p>
         </div>
