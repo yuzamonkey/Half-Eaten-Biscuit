b387946475f4fd664c9fee23ef8722415afab185
Jusa Mankki <jusamankki@gmail.com>
2021-07-30 14:04:27 +0300

    smooth scroll home page navigation

--- client/src/components/Pages/Home/Home.tsx
+++ client/src/components/Pages/Home/Home.tsx
@@ -5,36 +5,37 @@ import Guitarist from '../../../images/beard-musician-guitar-acoustic.jpeg'
 const Home = () => {
 
   return (
-    <div id="home" className="home-container">
-      <div className="home-page-navigation-container">
+    <div>
+      <nav className="home-page-navigation">
         <div>
-          <a href="#home">Top</a>
-          <a href="#about">About</a>
-          <a href="#features">Features</a>
+          <a href="#home" className="home-navigation-link">Top</a>
+          <a href="#about" className="home-navigation-link">About</a>
+          <a href="#features" className="home-navigation-link">Features</a>
         </div>
         <div>
-          <a href="/signin" className="link signin">Sign In</a>
-          <a href="/signup" className="link signup">Sign Up</a>
+          <a href="/signin" className="home-navigation-link">Sign In</a>
+          <a href="/signup" className="home-navigation-link">Sign Up</a>
+        </div>
+      </nav>
+      <div className="scroll-container">
+        <div className="scroll-page title-section" id="home">
+          <div className="title-container">
+            <h1 className="title">Hey freelancer, <br></br>you are needed!</h1>
+          </div>
+        </div>
+        <div className="scroll-page about-section" id="about">
+          <h1>About</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non adipisci tempora necessitatibus molestiae eius ea tenetur quibusdam delectus, unde et, sint, itaque assumenda iusto modi. Voluptates architecto non, fugit cupiditate assumenda earum, libero temporibus nostrum unde qui ea totam ut, iure eligendi voluptas labore! Vel ratione enim ad autem! Blanditiis consequuntur ab perspiciatis. Incidunt architecto accusantium repellendus error laudantium delectus eius repudiandae, optio corporis quasi ut provident libero ex ducimus fugit aut id alias recusandae eaque voluptate fuga neque quod. Cumque aperiam assumenda illo rerum facere minima, labore ad amet molestias? Voluptate eligendi aliquid, repellat esse a sed ipsum qui?</p>
+        </div>
+        <div className="scroll-page features-section" id="features">
+          <h1>Features</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ut ipsa nesciunt quod sequi pariatur, cumque, unde esse earum aliquid officiis eligendi? Atque aspernatur sequi tenetur aut qui, incidunt numquam reiciendis neque, laudantium officia deserunt non, eius illum facilis magnam nihil dolor enim. Incidunt repellat ipsum sequi nihil dolor quas obcaecati, necessitatibus iusto eum. Quo voluptatem aspernatur harum totam! Beatae maxime fugit corrupti! Eveniet hic totam doloribus eos recusandae! Quidem culpa tenetur consequatur rem aut voluptatem illo, fugit quaerat quo, quis assumenda tempore asperiores architecto adipisci cum. Repudiandae voluptate exercitationem fugit magnam! Ab ipsa minima eveniet odit amet doloremque soluta!</p>
+        </div>
+        <div className="footer-section">
+          <h1>Footer</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, a assumenda eveniet deleniti itaque amet, vero excepturi alias voluptate cum at obcaecati beatae dolor aperiam tenetur ipsum impedit debitis officia sapiente! Id officiis ex mollitia veritatis. Ullam dignissimos impedit nemo culpa, non sunt perferendis modi officia odit molestiae repellendus? Laboriosam amet quos aspernatur? Rem iste, dolores officiis in, magni neque incidunt minus iusto pariatur eveniet error provident nobis nam animi ullam voluptates velit nihil reiciendis temporibus aut molestias. Quos voluptatibus praesentium, ut nam saepe culpa nisi iusto nostrum earum magni asperiores aspernatur? Voluptas officia fugit in vitae esse sunt placeat.</p>
         </div>
       </div>
-      <div className="home-page-image-and-title-container">
-        <div className="title-container">
-          <h1 className="title">Hey freelancer, <br></br>you are needed!</h1>
-        </div>
-      </div>
-      <div id="about" className="home-text-section-container about-section-container">
-        <h1>About</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non adipisci tempora necessitatibus molestiae eius ea tenetur quibusdam delectus, unde et, sint, itaque assumenda iusto modi. Voluptates architecto non, fugit cupiditate assumenda earum, libero temporibus nostrum unde qui ea totam ut, iure eligendi voluptas labore! Vel ratione enim ad autem! Blanditiis consequuntur ab perspiciatis. Incidunt architecto accusantium repellendus error laudantium delectus eius repudiandae, optio corporis quasi ut provident libero ex ducimus fugit aut id alias recusandae eaque voluptate fuga neque quod. Cumque aperiam assumenda illo rerum facere minima, labore ad amet molestias? Voluptate eligendi aliquid, repellat esse a sed ipsum qui?</p>
-      </div>
-      <div id="features" className="home-text-section-container features-section-container">
-        <h1>Features</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ut ipsa nesciunt quod sequi pariatur, cumque, unde esse earum aliquid officiis eligendi? Atque aspernatur sequi tenetur aut qui, incidunt numquam reiciendis neque, laudantium officia deserunt non, eius illum facilis magnam nihil dolor enim. Incidunt repellat ipsum sequi nihil dolor quas obcaecati, necessitatibus iusto eum. Quo voluptatem aspernatur harum totam! Beatae maxime fugit corrupti! Eveniet hic totam doloribus eos recusandae! Quidem culpa tenetur consequatur rem aut voluptatem illo, fugit quaerat quo, quis assumenda tempore asperiores architecto adipisci cum. Repudiandae voluptate exercitationem fugit magnam! Ab ipsa minima eveniet odit amet doloremque soluta!</p>
-      </div>
-      <div className="footer-container">
-        <h1>Footer</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, a assumenda eveniet deleniti itaque amet, vero excepturi alias voluptate cum at obcaecati beatae dolor aperiam tenetur ipsum impedit debitis officia sapiente! Id officiis ex mollitia veritatis. Ullam dignissimos impedit nemo culpa, non sunt perferendis modi officia odit molestiae repellendus? Laboriosam amet quos aspernatur? Rem iste, dolores officiis in, magni neque incidunt minus iusto pariatur eveniet error provident nobis nam animi ullam voluptates velit nihil reiciendis temporibus aut molestias. Quos voluptatibus praesentium, ut nam saepe culpa nisi iusto nostrum earum magni asperiores aspernatur? Voluptas officia fugit in vitae esse sunt placeat.</p>
-      </div>
-
     </div>
   )
 }
