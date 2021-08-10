076d9e9f9b730827bf7cad898ff7f7ea90614816
Jusa Mankki <jusamankki@gmail.com>
2021-08-10 15:33:50 +0300

    jobquery subscription log on client

--- client/src/components/Navbar/Dropdowns/NotificationsDropdown.tsx
+++ client/src/components/Navbar/Dropdowns/NotificationsDropdown.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import './Dropdown.css'
+import { useSubscription } from '@apollo/client'
 
-const NotificationsDropdown = ({show, setShow}: any) => {
+import './Dropdown.css'
+import { JOBQUERY_ADDED } from '../../../graphql/subscriptions'
+
+const NotificationsDropdown = ({ show, setShow }: any) => {
+
+  useSubscription(JOBQUERY_ADDED, {
+    onSubscriptionData: async ({ subscriptionData }) => {
+      console.log("SUBSCRIPTION DATA ON NOTIFICATIONS DROPDOWN:\n", subscriptionData)
+    },
+  })
+
   if (!show) {
     return null
   }
+
   return (
     <div className="dropdown">
       <h3 className="notifications-title">Notifications</h3>
