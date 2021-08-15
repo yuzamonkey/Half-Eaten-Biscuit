abd08aa3c0ba1bf7aeeffb46ef5d566c54fd3b3a
Jusa Mankki <jusamankki@gmail.com>
2021-08-15 18:09:06 +0300

    add jobdetails component

--- client/src/components/Pages/Jobmarket/Pages/JobInfo.tsx
+++ client/src/components/Pages/Jobmarket/Pages/JobInfo.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom"
 import { FIND_JOBQUERY } from "../../../../graphql/queries";
-import { LargeProfileImage, Loading } from "../../../UtilityComponents/UtilityComponents";
+import { Loading } from "../../../UtilityComponents/UtilityComponents";
+import JobDetails from "./JobDetails";
 
 const JobInfo = () => {
   const { id }: any = useParams();
@@ -16,37 +17,11 @@ const JobInfo = () => {
   if (!jobInfoResult.data) {
     return <h1>Not found. The post might not be visible, or the poster has removed this query.</h1>
   }
-  const query = jobInfoResult.data.findJobquery
+  const job = jobInfoResult.data.findJobquery
 
   return (
-    <div>
-      <LargeProfileImage image={query.postedBy.object.profile.image} />
-      <div className="general-info-container">
-        <div className="image-and-name-container">
+    <JobDetails job={job}/>
 
-          <p><b>{query.postedBy.object.username || query.postedBy.object.profile.name}</b> is looking for <br />
-            {query.wantedCategories.map(category => category.object.name)}
-          </p>
-
-        </div>
-        {/* <p>{query.content}</p> */}
-        <div className="details-container">
-          <div className="details-item">
-            <p>Salary</p>
-            <p className="details-value">{query.salary}</p>
-          </div>
-          <div className="details-item">
-            <p>Location</p>
-            <p className="details-value">{query.location}</p>
-          </div>
-          <div className="details-item">
-            <p>Schedule</p>
-            <p className="details-value">{query.startSchedule.substring(0, 10)} •••  {query.endSchedule.substring(0, 10)}</p>
-          </div>
-        </div>
-      </div>
-      {query.content}
-    </div>
   )
 }
 
