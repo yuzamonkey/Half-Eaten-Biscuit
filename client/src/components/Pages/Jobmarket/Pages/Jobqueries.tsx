969ec0440a9ad7976b5a8eea07b458ef65b195f7
Jusa Mankki <jusamankki@gmail.com>
2021-08-15 19:07:25 +0300

    add styling to jobinfo

--- client/src/components/Pages/Jobmarket/Pages/Jobqueries.tsx
+++ client/src/components/Pages/Jobmarket/Pages/Jobqueries.tsx
@@ -4,7 +4,6 @@ import { useQuery } from '@apollo/client';
 import '../Jobmarket.css'
 import { ALL_JOBQUERIES } from '../../../../graphql/queries';
 import { Button, Loading } from '../../../UtilityComponents/UtilityComponents';
-import { useHistory } from 'react-router-dom';
 import { dateAsDDMMYYYY } from '../../../../utils/utilityFunctions';
 import JobDetails from './JobDetails';
 
@@ -13,7 +12,6 @@ interface Jobquery {
 }
 
 const Jobqueries = () => {
-  const history = useHistory()
   const result = useQuery(ALL_JOBQUERIES, {
     onCompleted: (data) => {
       setOrderedQueries([...data.allJobqueries].sort((q1, q2) => new Date(q2.postedOn).getTime() - new Date(q1.postedOn).getTime()))
@@ -52,55 +50,61 @@ const Jobqueries = () => {
   }
 
   return (
-    <div className="jobqueries-container">
+    <div>
       <div className={showJobInfo ? "job-details-container active" : "job-details-container"}>
-        <button onClick={() => setShowJobInfo(false)}>Back</button>
-        <JobDetails job={selectedJob} />
+        <div className="job-details-content">
+          <button onClick={() => setShowJobInfo(false)}>Back</button>
+          <JobDetails job={selectedJob} />
+        </div>
       </div>
-      Filter by: skill, group, posted on date, schedule, location
-      <label>Order by </label>
-      <select onChange={(e) => handleSelectedOrderChange(e.target.value)}>
-        {orderOptions.map(option => <option key={option}>{option}</option>)}
-      </select>
 
-      <ul>
-        {orderedQueries.map((q: any) => {
-          const contactText = `Contact ${q.postedBy.object.profile.firstName || q.postedBy.object.profile.name}`
-          return (
-            <div className="card" key={q.id}>
-              <div className="general-info-container">
-                <div className="image-and-name-container">
-                  <div className="image-container">
-                    <img src={q.postedBy.object.profile.image} alt="profileimg" className="jobquery-user-profile-image"></img>
+      <div className={showJobInfo ? "jobqueries-container" : "jobqueries-container active"}>
+        Filter by: skill, group, posted on date, schedule, location
+        <label>Order by </label>
+        <select onChange={(e) => handleSelectedOrderChange(e.target.value)}>
+          {orderOptions.map(option => <option key={option}>{option}</option>)}
+        </select>
+
+        <ul>
+          {orderedQueries.map((q: any) => {
+            const contactText = `Contact ${q.postedBy.object.profile.firstName || q.postedBy.object.profile.name}`
+            return (
+              <div className="card" key={q.id}>
+                <div className="general-info-container">
+                  <div className="image-and-name-container">
+                    <div className="image-container">
+                      <img src={q.postedBy.object.profile.image} alt="profileimg" className="jobquery-user-profile-image"></img>
+                    </div>
+                    <p><b>{q.postedBy.object.profile.name}</b> is looking for <br />
+                      {q.wantedCategories.map(category => category.object.profession || category.object.name)}
+                    </p>
+
                   </div>
-                  <p><b>{q.postedBy.object.profile.name}</b> is looking for <br />
-                    {q.wantedCategories.map(category => category.object.profession || category.object.name)}
-                  </p>
-
+                  <div className="details-container">
+                    <div className="details-item">
+                      <p>Salary</p>
+                      <p className="details-value">{q.salary}</p>
+                    </div>
+                    <div className="details-item">
+                      <p>Location</p>
+                      <p className="details-value">{q.location}</p>
+                    </div>
+                    <div className="details-item">
+                      <p>Schedule</p>
+                      <p className="details-value">{dateAsDDMMYYYY(q.startSchedule)} - {dateAsDDMMYYYY(q.endSchedule)}</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="details-container">
-                  <div className="details-item">
-                    <p>Salary</p>
-                    <p className="details-value">{q.salary}</p>
-                  </div>
-                  <div className="details-item">
-                    <p>Location</p>
-                    <p className="details-value">{q.location}</p>
-                  </div>
-                  <div className="details-item">
-                    <p>Schedule</p>
-                    <p className="details-value">{dateAsDDMMYYYY(q.startSchedule)} - {dateAsDDMMYYYY(q.endSchedule)}</p>
-                  </div>
+                <div className="buttons-container">
+                  <Button text={contactText} handleClick={() => console.log("CONTACT SOMEBODY")} />
+                  <Button text="More info" handleClick={() => handleMoreInfoClick(q)} />
                 </div>
               </div>
-              <div className="buttons-container">
-                <Button text={contactText} handleClick={() => console.log("CONTACT SOMEBODY")} />
-                <Button text="More info" handleClick={() => handleMoreInfoClick(q)} />
-              </div>
-            </div>
-          )
-        })}
-      </ul>
+
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 };
