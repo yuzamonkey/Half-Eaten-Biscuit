285da013a347572143c285ea40a0d8823482561e
Jusa Mankki <jusamankki@gmail.com>
2021-08-17 15:25:40 +0300

    add filesize limit

--- server/utils/server.ts
+++ server/utils/server.ts
@@ -1,21 +1,13 @@
 import express from 'express';
-//import bodyParser from 'body-parser';
 import { createConfig } from './apolloConfig';
 
-//const bodyParser = require('body-parser')
-//const cors = require('cors')
 
 const http = require('http')
 const app = express()
 const { ApolloServer } = require('apollo-server-express')
-//const { ApolloServer } = require('apollo-server')
 
-//app.use(cors)
-
-// app.use(bodyParser.json({
-//   json: {limit: '50mb', extended: true},
-//   urlencoded: {limit: '50mb', extended: true}
-// }))
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({limit: '1mb'}));
 
 const frontendRoutes = [
   '/',
