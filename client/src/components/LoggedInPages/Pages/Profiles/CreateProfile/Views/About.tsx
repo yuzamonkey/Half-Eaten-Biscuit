dda4fb0c9830e5b436e4280778208006454bfe1d
Jusa Mankki <jusamankki@gmail.com>
2021-09-03 11:32:41 +0300

    add about css

--- client/src/components/LoggedInPages/Pages/Profiles/CreateProfile/Views/About.tsx
+++ client/src/components/LoggedInPages/Pages/Profiles/CreateProfile/Views/About.tsx
@@ -1,12 +1,10 @@
 const About = ({ text, setText }: any) => {
   return (
-    <div>
-      <h2>Describe yourself</h2>
+    <div className="write-about-me-text-container">
+      <h2>Tell about yourself</h2>
       <textarea
         value={text}
         onChange={e => setText(e.target.value)}
-        cols={50}
-        rows={30}
       >
       </textarea>
     </div>
