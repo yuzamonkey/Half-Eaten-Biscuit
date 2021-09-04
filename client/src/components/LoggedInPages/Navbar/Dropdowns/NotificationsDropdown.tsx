5b66055eea2e569bb50ac3cdb57d13bee4402650
Jusa Mankki <jusamankki@gmail.com>
2021-09-04 15:43:17 +0300

    notifications bug fixing (fallback to this commit)

--- client/src/components/LoggedInPages/Navbar/Dropdowns/NotificationsDropdown.tsx
+++ client/src/components/LoggedInPages/Navbar/Dropdowns/NotificationsDropdown.tsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useLazyQuery, useMutation } from '@apollo/client'
+import { useLazyQuery, useMutation, useQuery } from '@apollo/client'
 import { useHistory } from 'react-router-dom'
 
 import './Dropdown.css'
 import { GET_NOTIFICATIONS } from '../../../../graphql/queries'
 import { UserContext } from '../../../UtilityComponents/UserContext'
 import { SET_NOTIFICATION_AS_SEEN, SET_ALL_NOTIFICATIONS_AS_SEEN } from '../../../../graphql/mutations'
+import { Loading } from '../../../UtilityComponents/UtilityComponents'
 
 interface INotification {
   id: string,
@@ -22,43 +23,73 @@ const NotificationsDropdown = ({ show, setShow, hasUnseenNotifications, setHasUn
   const history = useHistory()
   const userContext = useContext(UserContext)
   const [notifications, setNotifications] = useState<INotification[]>([])
-  const [getNotifications, { data }] = useLazyQuery(GET_NOTIFICATIONS)
+
+  //const [getNotifications, { data }] = useLazyQuery(GET_NOTIFICATIONS)
+  const notificationsResult = useQuery(GET_NOTIFICATIONS, {
+    variables: {
+      id: userContext.sessionId
+    },
+    onCompleted: (data) => {
+      console.log("N", data)
+      //const notifications = data.findUserOrGroup.notifications
+      // const sorted = sortNotifications(notifications)
+      // setNotifications(sorted)
+    }
+  })
+
   const [setNotificationAsSeen] = useMutation(SET_NOTIFICATION_AS_SEEN)
   const [setAllNotificationsAsSeen] = useMutation(SET_ALL_NOTIFICATIONS_AS_SEEN)
-  
-  //NOT WORKING
-  useEffect(() => {
-    getNotifications({
-      variables: { id: userContext.sessionId },
+
+  const sortNotifications = (notifications) => {
+    const sorted = [...notifications].sort((n1: any, n2: any) => {
+      const n1date = new Date(n1.object.date).getTime()
+      const n2date = new Date(n2.object.date).getTime()
+      return n2date - n1date
     })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userContext.sessionId])
+    return sorted
+  }
 
-  useEffect(() => {
-    if (data) {
-      const sorted = [...data.findUserOrGroup.notifications].sort((n1: any, n2: any) => {
-        const n1date = new Date(n1.object.date).getTime()
-        const n2date = new Date(n2.object.date).getTime()
-        return n2date - n1date
-      })
-      setNotifications(sorted)
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }
-  }, [data])
+  if (notificationsResult.loading) {
+    return (
+      <div className={show ? "dropdown active" : "dropdown"}>
+        <Loading />
+      </div>
+    )
+  }
+  //NOT WORKING
+  // useEffect(() => {
+  //   getNotifications({
+  //     variables: { id: userContext.sessionId },
+  //   })
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [userContext.sessionId])
 
-  useEffect(() => {
-    if (notifications) {
-      let hasUnseenNotifications = false
-      for (let n of notifications) {
-        if (!n.seen) {
-          hasUnseenNotifications = true
-          break
-        }
-      }
-      setHasUnseenNotifications(hasUnseenNotifications)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [notifications])
+
+  // useEffect(() => {
+  //   if (data) {
+  //     const sorted = [...data.findUserOrGroup.notifications].sort((n1: any, n2: any) => {
+  //       const n1date = new Date(n1.object.date).getTime()
+  //       const n2date = new Date(n2.object.date).getTime()
+  //       return n2date - n1date
+  //     })
+  //     setNotifications(sorted)
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   }
+  // }, [data])
+
+  // useEffect(() => {
+  //   if (notifications) {
+  //     let hasUnseenNotifications = false
+  //     for (let n of notifications) {
+  //       if (!n.seen) {
+  //         hasUnseenNotifications = true
+  //         break
+  //       }
+  //     }
+  //     setHasUnseenNotifications(hasUnseenNotifications)
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [notifications])
 
   // useSubscription(NOTIFICATION_ADDED, {
   //   onSubscriptionData: async ({ subscriptionData }) => {
--- documentation/working_hours.md
+++ documentation/working_hours.md
@@ -217,5 +217,5 @@
 | 31.8.  | 7.5 | Messaging subscription handling, bug fixes, css |
 | 1.9.  | 4.5 | css, refactoring, settings navigation |
 | 3.9.  | 7 | css |
-| 4.9.  | 945-13 | css |
+| 4.9.  | 945-1330, 1530- | css, misc, notifications bug |
 | =     | 185.0      |               |
