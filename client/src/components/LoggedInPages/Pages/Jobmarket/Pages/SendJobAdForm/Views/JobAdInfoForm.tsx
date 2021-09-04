f479e8121041b2e2422bcd42b5dd053c2dabfc6e
Jusa Mankki <jusamankki@gmail.com>
2021-09-04 10:20:40 +0300

    jobad info form css and html

--- client/src/components/LoggedInPages/Pages/Jobmarket/Pages/SendJobAdForm/Views/JobAdInfoForm.tsx
+++ client/src/components/LoggedInPages/Pages/Jobmarket/Pages/SendJobAdForm/Views/JobAdInfoForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const JobAdInfoForm = ({
   content,
   setContent,
@@ -10,43 +12,77 @@ const JobAdInfoForm = ({
   endSchedule,
   setEndSchedule,
 }) => {
+
+  const [showSalaryInput, setShowSalaryInput] = useState(false)
+
+  const handleSalaryChange = (value) => {
+    if (value === 'Yes, give value') {
+      setSalary('')
+      setShowSalaryInput(true)
+    } else {
+      setShowSalaryInput(false)
+      setSalary(value)
+    }
+  }
+
   return (
-    <div>
-      <h1>
-        Job Ad info form
-      </h1>
+    <div className="job-ad-info-form-container">
+
       <div>
-        description <textarea
+        <h2 className="secondary-text">Description</h2>
+        <textarea
           cols={50}
-          rows={5}
+          rows={10}
           value={content}
           onChange={({ target }) => setContent(target.value)}
         />
       </div>
+
       <div>
-        location <input
-          value={location}
-          onChange={({ target }) => setLocation(target.value)}
-        />
+        <div>
+          <h2 className="secondary-text">Starting</h2>
+          <input
+            type="date"
+            value={startSchedule}
+            onChange={({ target }) => setStartSchedule(target.value)}
+          />
+        </div>
+        <div>
+          <h2 className="secondary-text">Ending</h2>
+          <input
+            type="date"
+            value={endSchedule}
+            onChange={({ target }) => setEndSchedule(target.value)}
+          />
+        </div>
+        <div>
+          <h2 className="secondary-text">Location</h2>
+          <input
+            value={location}
+            onChange={({ target }) => setLocation(target.value)}
+          />
+        </div>
+        <div>
+          <h2 className="secondary-text">Salary</h2>
+          <select onChange={({ target }) => handleSalaryChange(target.value)}>
+            <option>Yes</option>
+            <option>Yes, give value</option>
+            <option>Negotiable</option>
+            <option>No</option>
+          </select>
+          {showSalaryInput &&
+            <>
+                <input
+                  type="number"
+                  value={salary}
+                  onChange={({ target }) => setSalary(target.value)}
+                />
+                €
+            </>
+          }
+        </div>
       </div>
-      <div>
-        salary <input
-          value={salary}
-          onChange={({ target }) => setSalary(target.value)}
-        />
-      </div>
-      <div>
-        startSchedule (or Day, Month, Year dropdowns?)<input
-          value={startSchedule}
-          onChange={({ target }) => setStartSchedule(target.value)}
-        />
-      </div>
-      <div>
-        endSchedule <input
-          value={endSchedule}
-          onChange={({ target }) => setEndSchedule(target.value)}
-        />
-      </div>
+
     </div>
   )
 }
